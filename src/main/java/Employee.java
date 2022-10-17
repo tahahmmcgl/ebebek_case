eb5722ddbc2457130e93819049f5a0f0590c5801
Taha <tahahmmcgl@gmail.com>
2022-10-17 16:18:03 +0300

    All case is done

---
+++ src/main/java/Employee.java
@@ -0,0 +1,50 @@
+public class Employee {
+
+    private String name;
+    private double salary;
+    private int workHours,hireYear;
+
+    Employee(String name, double salary, int workHours, int hireYear){
+        this.name=name;
+        this.salary=salary;
+        this.hireYear=hireYear;
+        this.workHours=workHours;
+    }
+    private double tax(){
+        double tax=0;
+        if (this.salary>=1000){
+            tax=this.salary*0.3;
+        }
+        return tax;
+    }
+    private double bonus(){
+        double bonus=0;
+        if(this.workHours>=40){
+            bonus=(this.workHours-40)*30;
+        }
+        return bonus;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                ", workHours=" + workHours +
+                ", hireYear=" + hireYear +
+                '}';
+    }
+
+    private double raiseSalary(){
+        int year=2021-this.hireYear;
+        double totalSalary=(this.salary-this.tax()+this.bonus());
+        if (year<10){
+            return totalSalary*0.05;
+        } else if (year>=10 && year<20) {
+            return totalSalary*0.1;
+        }else {
+            return totalSalary*0.15;
+        }
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+
+
+public class Main {
+    public static void main(String[] args){
+        Employee emp1=new Employee("Taha",10000,45,2020);
+        System.out.println(emp1.toString());
+    }
+}
